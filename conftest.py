7ce4abaf26c33375128372a516aa7bdeac180164
Umair Habib <umairhabib000@gmail.com>
2022-09-11 17:36:16 +0500

    User | Profile testing

---
+++ conftest.py
@@ -0,0 +1,35 @@
+# fixtures are run before test function
+# these are used to provide input params or db urls etc
+
+
+import pytest
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "real_estate.settings.development")
+import django
+django.setup()
+
+from pytest_factoryboy import register
+from tests.factories import ProfileFactory, UserFactory
+
+
+register(ProfileFactory)
+register(UserFactory)
+
+
+@pytest.fixture
+def base_user(db, user_factory):
+    new_user = user_factory.create()
+    return new_user
+
+
+@pytest.fixture
+def super_user(db, user_factory):
+    new_user = user_factory.create(is_staff=True, is_superuser=True)
+    return new_user
+
+
+@pytest.fixture
+def profile(db, profile_factory):
+    user_profile = profile_factory.create()
+    return user_profile
+
--- real_estate/asgi.py
+++ real_estate/asgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "real_estate.settings")
+django.setup()
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "real_estate.settings")
 
 application = get_asgi_application()
