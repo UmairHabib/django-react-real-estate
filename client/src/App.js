1f515b656fcc74a0180b5483880e4a2c843e95fc
Umair Habib <umairhabib000@gmail.com>
2022-09-20 16:26:42 +0500

    Nginx React setup with Docker

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from "react";
+function App() {
+  return (
+    <div>
+      <h1> Real Estate </h1>
+      <p> real estate 3 test</p>
+    </div>
+  );
+}
+
+export default App;
