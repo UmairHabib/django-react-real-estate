a5268e47c514e143bc3f61cfa5a2df91f4846071
Umair Habib <umairhabib000@gmail.com>
2022-09-21 23:32:59 +0500

    Static Frontend base setup using react and antd

--- client/src/reducers/propertyReducers.js
+++ client/src/reducers/propertyReducers.js
@@ -1,5 +1,15 @@
+import { PROPERTY_LIST_FAIL, PROPERTY_LIST_SUCCESS, PROPERTY_LIST_REQUEST } from '../actions/types';
+
+
 export const propertiesListReducer = (state={properties: []}, action)=>{
     switch(action.type){
+
+        case PROPERTY_LIST_REQUEST:
+            return {loading:true, properties:[]}
+        case PROPERTY_LIST_SUCCESS:
+            return {loading:false, properties:action.payload.results} 
+        case PROPERTY_LIST_FAIL:
+            return {loading:false, properties:action.payload}
         default:
             return state;
     }
